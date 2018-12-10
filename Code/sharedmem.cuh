ec4054b99ce65fb694da6350d2c7682a5a9d060c
Anjum Sohail <itdanjum@gmail.com>
2018-12-10 14:52:44 +0500

    Performance Tunning

--- Code/sharedmem.cuh
+++ Code/sharedmem.cuh
@@ -1,25 +1,3 @@
-/*
- * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
- 
- /*
-* Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
-*
-* Please refer to the NVIDIA end user license agreement (EULA) associated
-* with this source code for terms and conditions that govern your use of
-* this software. Any use, reproduction, disclosure, or distribution of
-* this software and related documentation outside the terms of the EULA
-* is strictly prohibited.
-*
-*/
-
 #ifndef _SHAREDMEM_H_
 #define _SHAREDMEM_H_
 

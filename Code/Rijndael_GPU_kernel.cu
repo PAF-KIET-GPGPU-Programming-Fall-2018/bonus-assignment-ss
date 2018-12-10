d8c31818b4828f18115ce54da3e1ace83783700a
Anjum Sohail <itdanjum@gmail.com>
2018-12-10 14:49:44 +0500

    Performance Tunning

--- Code/Rijndael_GPU_kernel.cu
+++ Code/Rijndael_GPU_kernel.cu
@@ -1,16 +1,7 @@
-/*
- * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
 
-/* This sample is a templatized version of the template project.
- * It also shows how to correctly templatize dynamically allocated shared
+
+/* 
+ * The Code below shows how to correctly templatize dynamically allocated shared
  * memory arrays.
  * Device code.
  */
